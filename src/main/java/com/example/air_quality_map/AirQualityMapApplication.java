bc50a6726728bfab9114c3184a9d3b65702b51c3
SHIN SUJUNG <shinssjj79@gmail.com>
2024-10-12 08:53:22 +0900

    Initial commit

---
+++ src/main/java/com/example/air_quality_map/AirQualityMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.air_quality_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/air_quality_map/AirQualityMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.air_quality_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
